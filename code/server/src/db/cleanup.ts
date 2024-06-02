20d84da06d27cd1a69dbfa319cd8bea34f8c84e9
FEDERICO SCIASCIA S333188 <s333188@studenti.polito.it>
2024-06-02 13:58:38 +0200

    [TMP] cleanupDB

--- code/server/src/db/cleanup.ts
+++ code/server/src/db/cleanup.ts
@@ -9,12 +9,38 @@ import db from "../db/db";
 
 export function cleanup() {
     db.serialize(() => {
+        db.run("DELETE FROM prod_in_cart")
+        db.run("DELETE FROM reviews")
+        db.run("DELETE FROM carts")
+        db.run("DELETE FROM products")
         // Delete all data from the database.
         db.run("DELETE FROM users")
-        //Add delete statements for other tables here
-        db.run("DELETE FROM carts")
-        db.run("DELETE FROM prod_in_cart")
-        db.run("DELETE FROM products")
-        db.run("DELETE FROM reviews")
+        //Add delete statements for other tables here 
     })
+}
+
+export function cleanupDB(): Promise<void> {
+    return new Promise<void>((resolve, reject) => {
+        db.serialize(() => {
+            db.run("DELETE FROM prod_in_cart", (err) => {
+                if (err) reject(err);
+            });
+            db.run("DELETE FROM reviews", (err) => {
+                if (err) reject(err);
+            });
+            db.run("DELETE FROM carts", (err) => {
+                if (err) reject(err);
+            });
+            db.run("DELETE FROM products", (err) => {
+                if (err) reject(err);
+            });
+            db.run("DELETE FROM users", (err) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve();
+                }
+            });
+        });
+    });
 }
