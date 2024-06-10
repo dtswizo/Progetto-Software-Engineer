4da5b98caeb61579c73f7b9c2a4bfeaecd85f420
Davide Licitra s331360 <s331360@studenti.polito.it>
2024-06-10 09:46:27 +0200

    [EDIT] Try Catch added on ReviewDao

--- code/server/src/dao/reviewDAO.ts
+++ code/server/src/dao/reviewDAO.ts
@@ -6,108 +6,131 @@ import { ProductNotFoundError } from "../errors/productError";
 
 class ReviewDAO {
     async addReview(model: string, user: User, score: number, comment: string): Promise<void> {
-        return new Promise<void>((resolve, reject) => {
-            const sql = "INSERT INTO reviews(user, model, score, date, comment) VALUES(?, ?, ?, ?, ?)";
-            const date = new Date().toISOString().slice(0, 10);
+        try {
+            return new Promise<void>((resolve, reject) => {
+                const sql = "INSERT INTO reviews(user, model, score, date, comment) VALUES(?, ?, ?, ?, ?)";
+                const date = new Date().toISOString().slice(0, 10);
 
-            db.run(sql, [user.username, model, score, date, comment], (err: Error | null) => {
-                if (err) {
-                    if (err.message.includes("UNIQUE constraint failed: reviews.user, reviews.model")) {
-                        reject(new ExistingReviewError());
-                    } else {
-                        reject(err);
+                db.run(sql, [user.username, model, score, date, comment], (err: Error | null) => {
+                    if (err) {
+                        if (err.message.includes("UNIQUE constraint failed: reviews.user, reviews.model")) {
+                            reject(new ExistingReviewError());
+                        } else {
+                            reject(err);
+                        }
+                        return;
                     }
-                    return;
-                }
-                resolve();
+                    resolve();
+                });
             });
-        });
+        } catch (error) {
+            throw error;
+        }
     }
 
     async getProductReviews(model: string): Promise<ProductReview[]> {
-        return new Promise<ProductReview[]>((resolve, reject) => {
-            const sql = "SELECT * FROM reviews WHERE reviews.model = ?";
-            db.all(sql, [model], (err: Error | null, rows: any[]) => {
-                if (err) {
-                    reject(err);
-                    return;
-                }
-                
-                //TODO: Questo errore va veramente ritornato?
-                if (rows.length === 0) {   //sta roba non so se va rimossa, cioè se il prodotto non ha recensioni va ritornato array vuoto? o reject
-                    resolve([]);
-                    return;
-                }
+        try {
+            return new Promise<ProductReview[]>((resolve, reject) => {
+                const sql = "SELECT * FROM reviews WHERE reviews.model = ?";
+                db.all(sql, [model], (err: Error | null, rows: any[]) => {
+                    if (err) {
+                        reject(err);
+                        return;
+                    }
+                    
+                    if (rows.length === 0) {
+                        resolve([]);
+                        return;
+                    }
 
-                const reviews: ProductReview[] = rows.map(row => {
-                    return new ProductReview(row.model, row.user, row.score, row.date, row.comment);
+                    const reviews: ProductReview[] = rows.map(row => {
+                        return new ProductReview(row.model, row.user, row.score, row.date, row.comment);
+                    });
+
+                    resolve(reviews);
                 });
-
-                resolve(reviews);
             });
-        });
+        } catch (error) {
+            throw error;
+        }
     }
 
     async deleteReview(model: string, user: User): Promise<void> {
-        return new Promise<void>((resolve, reject) => {
-            const sql = "DELETE FROM reviews WHERE model = ? AND user = ?";
-            db.run(sql, [model, user.username], function (err: Error | null) {
-                if (err) {
-                    reject(err);
-                    return;
-                }
+        try {
+            return new Promise<void>((resolve, reject) => {
+                const sql = "DELETE FROM reviews WHERE model = ? AND user = ?";
+                db.run(sql, [model, user.username], function (err: Error | null) {
+                    if (err) {
+                        reject(err);
+                        return;
+                    }
 
-                if (this.changes === 0) {
-                    reject(new NoReviewProductError());
-                    return;
-                }
+                    if (this.changes === 0) {
+                        reject(new NoReviewProductError());
+                        return;
+                    }
 
-                resolve();
+                    resolve();
+                });
             });
-        });
+        } catch (error) {
+            throw error;
+        }
     }
 
     async deleteReviewsOfProduct(model: string): Promise<void> {
-        return new Promise<void>((resolve, reject) => {
-            const sql = "DELETE FROM reviews WHERE model = ?";
-            db.run(sql, [model], function (err: Error | null) {
-                if (err) {
-                    reject(err);
-                    return;
-                }
-                resolve();
+        try {
+            return new Promise<void>((resolve, reject) => {
+                const sql = "DELETE FROM reviews WHERE model = ?";
+                db.run(sql, [model], function (err: Error | null) {
+                    if (err) {
+                        reject(err);
+                        return;
+                    }
+                    resolve();
+                });
             });
-        });
+        } catch (error) {
+            throw error;
+        }
     }
 
     async deleteAllReviews(): Promise<void> {
-        return new Promise<void>((resolve, reject) => {
-            const sql = "DELETE FROM reviews";
-            db.run(sql, [], function (err: Error | null) {
-                if (err) {
-                    reject(err);
-                    return;
-                }
-                resolve();
+        try {
+            return new Promise<void>((resolve, reject) => {
+                const sql = "DELETE FROM reviews";
+                db.run(sql, [], function (err: Error | null) {
+                    if (err) {
+                        reject(err);
+                        return;
+                    }
+                    resolve();
+                });
             });
-        });
+        } catch (error) {
+            throw error;
+        }
     }
 
     async productCheck(model: string): Promise<boolean> {
-        return new Promise<boolean>((resolve, reject) => {
-            const sql = "SELECT * FROM products WHERE model = ?";
-            db.all(sql, [model], (err: Error | null, rows: any[]) => {
-                if (err) {
-                    reject(err);
-                    return;
-                }
-                if (rows.length === 0) {
-                    reject(new ProductNotFoundError()); // Nessun prodotto trovato
-                } else {
-                    resolve(true);  // Almeno un prodotto trovato
-                }
+        try {
+            return new Promise<boolean>((resolve, reject) => {
+                const sql = "SELECT * FROM products WHERE model = ?";
+                db.all(sql, [model], (err: Error | null, rows: any[]) => {
+                    if (err) {
+                        reject(err);
+                        return;
+                    }
+                    if (rows.length === 0) {
+                        reject(new ProductNotFoundError()); // Nessun prodotto trovato
+                    } else {
+                        resolve(true);  // Almeno un prodotto trovato
+                    }
+                });
             });
-        });
+        } catch (error) {
+            throw error;
+        }
     }
 }
 
